43a7eba8acf26eecd0ede1f47fd4f54d38ce9d74
PrathikshaS11 <prathiksha112004@gmailcom>
2024-07-17 13:41:19 +0530

    js

---
+++ 02_js_variables/program3.js
@@ -0,0 +1,7 @@
+var score;
+score = 200; // Changed value
+console.log(score);
+
+var score2;
+score2 = 300; // New variable with value
+console.log(score2);
---
+++ 02_js_variables/program4.js
@@ -0,0 +1,13 @@
+var score;
+score = 100;
+console.log(score);
+
+score = 200; // Updated value
+console.log(score);
+
+var score2;
+score2 = 250; // Initial value
+console.log(score2);
+
+score2 = 300; // Updated value
+console.log(score2);

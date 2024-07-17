a6543d1062c202490952fd1eaf061abb40fb5ea7
PrathikshaS11 <prathiksha112004@gmailcom>
2024-07-17 13:50:38 +0530

    js

---
+++ 02_js_variables/program8.js
@@ -0,0 +1,16 @@
+var score;
+score = 100;
+console.log("Your score was " + score);
+
+console.log("Great splat!!!");
+score = score + 50;
+console.log("New score: " + score);
+
+console.log("Way to go!");
+
+// Further Adventures
+console.log("Successful splat of a kumquat!");
+score = score + 100;
+console.log("Updated score: " + score);
+console.log("Congratulations!");
+
